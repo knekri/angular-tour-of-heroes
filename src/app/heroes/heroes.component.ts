bfca67156eb1d993dc558289dd63305d7630ecc1
Krisztian Knehr <knehr.krisztian@gmail.com>
2020-05-09 11:34:09 +0200

    toh_display_list_of_heroes - https://angular.io/tutorial/toh-pt2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',                 // selector— the component's CSS element selector
@@ -11,10 +12,16 @@ export class HeroesComponent implements OnInit {
 
   hero: Hero = { id: 1, name: 'Windstorm' };
 
+  heroes = HEROES;
+  selectedHero: Hero;
+
   constructor() {
   }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
