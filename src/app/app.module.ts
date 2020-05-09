b72bbb9eada435b94c09fde2b4b6455ad59bd541
Krisztian Knehr <knehr.krisztian@gmail.com>
2020-05-09 12:04:14 +0200

    toh_move_the_hero_details_into_a_component - https://angular.io/tutorial/toh-pt3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 
 @NgModule({ // https://angular.io/tutorial/toh-pt1#appmodule
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  // https://angular.io/tutorial/toh-pt3#add-the-input-hero-property
+  @Input() hero: Hero;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
