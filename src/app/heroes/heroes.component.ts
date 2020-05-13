937b62b00110715a9f5296ae13a1a2fd68164d5b
Krisztian Knehr <knehr.krisztian@gmail.com>
2020-05-13 19:16:30 +0200

    toh_add_delete-hero_feature - https://angular.io/tutorial/toh-pt6#delete-a-hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -32,4 +32,11 @@ export class HeroesComponent implements OnInit {
     if (!name) { return; }
     this.heroService.addHero({name} as Hero).subscribe(hero => this.heroes.push(hero));
   }
+
+  delHero(hero: Hero): void {
+    this.heroes = this.heroes.filter(value => value !== hero);
+    // If you neglect to subscribe(), the service will not send the delete request to the server.
+    // As a rule, an Observable does nothing until something subscribes.
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
